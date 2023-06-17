9c72d47a724bf37470f541fd1c353fb05877dc3d
hunkz <hunkizzzx777@gmail.com>
2023-06-17 20:32:44 +0300

    init commit

--- rest-coinmarketcap-monitor-service/src/main/resources/db.changelog/sql/init.sql
+++ rest-coinmarketcap-monitor-service/src/main/resources/db.changelog/sql/init.sql
@@ -1,5 +1,5 @@
 -- liquibase formatted sql
 
 -- changeset hunkz:1687022548322-1
-CREATE TABLE crypto_currency (uuid UUID NOT NULL, name VARCHAR(255), price FLOAT(53) NOT NULL, symbol VARCHAR2(20), CONSTRAINT "crypto_currencyPK" PRIMARY KEY (uuid));
+CREATE TABLE CRYPTO_CURRENCY (uuid UUID NOT NULL, name VARCHAR(255), price FLOAT(53) NOT NULL, symbol VARCHAR(20), CONSTRAINT "crypto_currencyPK" PRIMARY KEY (uuid));
 
